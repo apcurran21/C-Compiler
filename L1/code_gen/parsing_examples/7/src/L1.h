5060e96632786cb1632430ed7363cd421b1f8532
apcurran21 <102324978+apcurran21@users.noreply.github.com>
2024-01-17 14:00:58 -0600

    moved my step-by-step files into the repo dir

---
+++ L1/code_gen/parsing_examples/7/src/L1.h
@@ -0,0 +1,102 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+namespace L1 {
+
+  // forward declaration of Program
+  class Program;
+
+  enum RegisterID {rdi, rax};
+
+  class Item {
+    public:
+      virtual int translate() = 0;
+  };
+
+  /*
+  Register = 0
+  Number = 1
+  Name = 2
+  Label = 3
+  */
+
+  class Register : public Item {
+    public:
+      Register (std::string r);
+      int translate() override;
+      std::string get_ID();
+    private:
+      std::string ID;
+  };
+
+  class Number : public Item {
+    public:
+      Number (int n);
+      int translate() override;
+      int get_value();
+    private:
+      int value;
+  };
+
+  class Name : public Item {
+    public:
+      Name (const std::string &value);
+      int translate() override;
+      std::string get_value();
+    private:
+      std::string value;
+  };
+
+  class Label : public Item {
+    public:
+      Label (const std::string &value);
+      int translate() override;
+      std::string get_value();
+    private:
+      std::string value;
+  };
+
+  /*
+   * Instruction interface.
+   */
+  class Instruction{
+    virtual void gen(Program &p, std::ofstream &outputFile) = 0;
+  };
+
+  /*
+   * Instructions.
+   */
+  class Instruction_ret : public Instruction{
+    void gen(Program &p, std::ofstream &outputFile) override;
+  };
+
+  class Instruction_assignment : public Instruction{
+    public:
+      Instruction_assignment (Item *dst, Item *src);
+      void gen(Program &p, std::ofstream &outputFile) override;
+    private:
+      Item *s;
+      Item *d;
+  };
+
+  /*
+   * Function.
+   */
+  class Function{
+    public:
+      std::string name;
+      int64_t arguments;
+      int64_t locals;
+      std::vector<Instruction *> instructions;
+  };
+
+  class Program{
+    public:
+      std::string entryPointLabel;
+      std::vector<Function *> functions;
+  };
+
+}
---
+++ L1/code_gen/parsing_examples/7/src/code_generator.cpp
@@ -0,0 +1,84 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include <code_generator.h>
+#include "L1.h"
+
+using namespace std;
+
+namespace L1{
+
+  /*
+    Assembly generating methods
+  */
+  void Instruction_ret::gen(Program &p, std::ofstream &outputFile) {
+    // return translates to retq
+    outputFile << "retq\n";
+  }
+
+  void Instruction_assignment::gen(Program &p, std::ofstream &outputFile) {
+    Register *dreg = dynamic_cast< Register * >(this->d);
+    std::string dreg_ID = dreg->get_ID();
+    int src_code = this->s->translate();
+    switch(src_code) {
+      case 0:
+        {
+          Register *sreg = dynamic_cast< Register * >(this->s);
+          outputFile << "movq %" << sreg->get_ID();
+          break;
+        }
+      case 1:
+        {
+          Number *num = dynamic_cast< Number * >(this->s);
+          outputFile << "movq $" << num->get_value();
+          break;
+        }
+      case 2:
+        {
+          Name *name = dynamic_cast< Name * >(this->s);
+          outputFile << "movq $_" << name->get_value();
+          break;
+        }
+      case 3:
+        {
+          Label *labl = dynamic_cast< Label * >(this->s);
+          outputFile << "movq $" << labl->get_value();
+          break;
+        }
+      default:
+        {
+          std::cerr << "something went wrong in instruction_assignment gen";
+        }
+    }
+
+    outputFile << ", %" << dreg_ID << "\n";
+  }
+
+
+  void generate_code(Program p){
+
+    /* 
+     * Open the output file.
+     */ 
+    std::ofstream outputFile;
+    outputFile.open("prog.S");
+   
+    /* 
+     * Generate target code
+     */ 
+
+    // boilerplate
+    outputFile << ".text\n.globl go\ngo:\npushq %rbx\npushq %rbx\npushq %r12\npushq %r13\npushq %r14\npushq %r15\n";
+    std::string entry_lab = p.entryPointLabel;
+    outputFile << "call " << entry_lab.replace(0, 1, "_") << "\n";
+    outputFile << "popq %r15\npopq %r14\npopq %r13\npopq %r12\npopq %rbp\npopq %rbx\nretq\n";
+
+    /* 
+     * Close the output file.
+     */ 
+    outputFile.close();
+   
+    return ;
+  }
+}
