bed91fb931140de29561a09825d616dcf885460e
apcurran21 <102324978+apcurran21@users.noreply.github.com>
2024-01-28 23:36:32 -0600

    finalized the variable allocator class

--- L2/src/variable_allocator.cpp
+++ L2/src/variable_allocator.cpp
@@ -3,4 +3,21 @@
 
 namespace L2 {
 
+    VariableAllocator::VariableAllocator() {}
+
+    Variable* VariableAllocator::allocate_variable(std::string var_name) {
+        auto loc = variable_database.find(var_name);
+
+        if (loc != variable_database.end()) {
+            // Variable was already allocated!
+            return variable_database[var_name];
+        }
+        else {
+            // Variable was not allocated, need to create
+            auto new_var = new Variable(var_name);
+            return new_var;     // new_var is a pointer 
+        }
+    }
+
+
 }
